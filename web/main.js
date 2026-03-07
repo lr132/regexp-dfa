b27756d7dccacfca40e94f29ac9d5a64e74942c2
Lewis <lmlr1g24@soton.ac.uk>
2026-03-07 20:29:41 +0000

    Use _initialize for GHC WASM reactor init

--- web/main.js
+++ web/main.js
@@ -108,7 +108,7 @@ async function init() {
       { ghc_wasm_jsffi: jsffiImports, wasi_snapshot_preview1: wasi }
     ));
     Object.assign(exports_ref, instance.exports);
-    await instance.exports.hs_init();
+    await instance.exports._initialize();
     hs = instance.exports;
 
     vizInstance = await Viz.instance();
